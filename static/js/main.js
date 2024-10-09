f7cc0951073ef451f5f3f3dfb90ce0469db74ffc
tahsin soufizade <soufilearn@gmail.com>
2024-10-09 15:08:17 +0330

    change burger button styles when menu is open

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,9 @@ const headerNavbar = document.getElementById("header-navbar");
 headerBurgerButton.addEventListener("click", () => {
   if (headerNavbar.classList.contains("-right-full")) {
     headerNavbar.classList.replace("-right-full", "right-0");
+    headerBurgerButton.classList.add("active");
   } else {
     headerNavbar.classList.replace("right-0", "-right-full");
+    headerBurgerButton.classList.remove("active");
   }
 });
